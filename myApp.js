67c4f28501941eac2700137cdf240db4db909454
MaestroAmu <tossysidney001@gmail.com>
2023-02-22 14:29:03 +0300

    Added personSchema

--- myApp.js
+++ myApp.js
@@ -3,8 +3,16 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 require('dotenv').config();
 
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods : [String]
+})
 
-let Person;
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
